163d48f88d09dce9956f65c45174d6920d763e91
卢云华 <luyh@59store.com>
2016-06-02 09:57:10 +0200

    container

---
+++ src/Tomato/Foundation/Container.php
@@ -0,0 +1,21 @@
+<?php
+namespace Tomato\Foundation;
+/**
+ * Created by luyh@59store.com.
+ * User: luyh
+ * Date: 16/6/2
+ * Time: 下午3:42
+ */
+class Container implements IContainer
+{
+
+    function get($key)
+    {
+        // TODO: Implement get() method.
+    }
+
+    function set($key, &$value)
+    {
+        // TODO: Implement set() method.
+    }
+}
---
+++ src/Tomato/Foundation/IContainer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by luyh@59store.com.
+ * User: luyh
+ * Date: 16/6/2
+ * Time: 下午3:50
+ */
+
+namespace Tomato\Foundation;
+
+
+interface IContainer
+{
+    function get($key);
+
+    function set($key, &$value);
+
+}
